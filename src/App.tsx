1fffda4c004b2c7ba441a803971f325597bcf0ad
adilkhansatemirov <adilkhansatemirov@gmail.com>
2024-06-15 08:26:50 +0300

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { DndContext } from "@dnd-kit/core";
 import Draggable from "./components/Draggable";
 import Droppable from "./components/Droppable";
@@ -7,11 +8,17 @@ function App() {
   return (
     <>
       <DndContext>
-        <Draggable />
-        <Droppable />
-        <p className="read-the-docs">
+        <Draggable>Draggable</Draggable>
+        <Droppable>Some random text</Droppable>
+        <p className="text-3xl font-bold underline">
           Click on the Vite and React logos to learn more
         </p>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<div>home</div>} />
+            <Route path="/about" element={<div>about</div>} />
+          </Routes>
+        </BrowserRouter>
       </DndContext>
     </>
   );
